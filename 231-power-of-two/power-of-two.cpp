2801d441aae1cdd90e540d83e97397ff70c4ec15
sakshamsuyal <mr.suyal24@gmail.com>
2025-10-30 22:53:24 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (16.48%) - LeetSync

---
+++ 231-power-of-two/power-of-two.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+       while(n>1 && n%2==0){
+        n=n/2;
+        
+       }
+       if(n==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+};
