6d0c89406f3a355698d7649ecef2f6b7954a4ed5
sakshamsuyal <mr.suyal24@gmail.com>
2025-12-29 23:59:04 +0530

    Time: 43 ms (68.56%) | Memory: 55.5 MB (5.74%) - LeetSync

--- 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
@@ -1,7 +1,11 @@
 /**
- * @param {...any} args
+ * @param {...(null|boolean|number|string|Array|Object)} args
  * @return {number}
  */
 var argumentsLength = function(...args) {
     return args.length;
 };
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
